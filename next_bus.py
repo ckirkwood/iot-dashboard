e7b05135a39667f0e7bd115181f00633e7e5140c
Callum Kirkwood <callum.kirkwood@gmail.com>
2017-04-16 13:05:46 +0100

    Move API query to for loop

--- next_bus.py
+++ next_bus.py
@@ -1,15 +1,13 @@
 import requests, json, dweepy, time
 
-r = requests.get('https://transportapi.com/v3/uk/bus/stop/1800NB04841/live.json?app_id=391561b5&app_key=0e3179907ec6f4ea2ca0a1f0b1c82c3f&group=route&nextbuses=yes')
-j = r.json()
-
-Line_33 = j['departures'][u'33'][0][u'aimed_departure_time']
-#Line_63 = j['departures'][u'63'][0][u'aimed_departure_time']
-
 while True:
+    r = requests.get('https://transportapi.com/v3/uk/bus/stop/1800NB04841/live.json?app_id=391561b5&app_key=0e3179907ec6f4ea2ca0a1f0b1c82c3f&group=route&nextbuses=yes')
+    j = r.json()
 #    dweepy.dweet_for('63Bus', {'time': Line_63})
 #    time.sleep(0.5)
+    Line_33 = j['departures'][u'33'][0][u'aimed_departure_time']
+    time.sleep(0.5)
     dweepy.dweet_for('33Bus', {'time': Line_33})
-    time.sleep(1200)
+    time.sleep(600)
 
 
