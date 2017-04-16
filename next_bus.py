3800b48c0f4d83894d83e605052257e5ed6013b2
Callum Kirkwood <callum.kirkwood@gmail.com>
2017-04-16 20:20:40 +0100

    Add step to check if bus service is running

--- next_bus.py
+++ next_bus.py
@@ -1,13 +1,24 @@
 import requests, json, dweepy, time
 
 while True:
-    r = requests.get('https://transportapi.com/v3/uk/bus/stop/1800NB04841/live.json?app_id=391561b5&app_key=0e3179907ec6f4ea2ca0a1f0b1c82c3f&group=route&nextbuses=yes')
-    j = r.json()
-#    dweepy.dweet_for('63Bus', {'time': Line_63})
-#    time.sleep(0.5)
-    Line_33 = j['departures'][u'33'][0][u'aimed_departure_time']
-    time.sleep(0.5)
-    dweepy.dweet_for('33Bus', {'time': Line_33})
-    time.sleep(600)
+## Query Transport API database, create dictionary from the output
+    j = requests.get('https://transportapi.com/v3/uk/bus/stop/1800NB04841/live.json?app_id=391561b5&app_key=0e3179907ec6f4ea2ca0a1f0b1c82c3f&group=route&nextbuses=yes').json()
+    d = json.dumps(j)
+    data = json.loads(d)
 
+## Check to see if values for each service are present before pushing data to Dweet.io
+    if '33' in data.get('departures') != True:
+        Line_33 = data['departures']['33'][0]['aimed_departure_time']
+        time.sleep(0.5)
+        dweepy.dweet_for('33Bus', {'time': Line_33})
+    else:
+    	dweepy.dweet_for('33Bus', {'time': 'No busses'})
+
+    if '63' in data.get('departures') != True:
+        Line_63 = data['departures']['63'][0]['aimed_departure_time']
+	time.sleep(0.5)
+        dweepy.dweet_for('63Bus', {'time': Line_63})
+    else:
+	dweepy.dweet_for('63Bus', {'time': 'No busses'})
+        time.sleep(300)
 
