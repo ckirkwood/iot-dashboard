19c73502876869226312cf5fa546e7523ab37d49
Callum Kirkwood <callum.kirkwood@gmail.com>
2017-04-17 12:01:53 +0100

    Update description

--- blinkt_arcade.py
+++ blinkt_arcade.py
@@ -1,4 +1,4 @@
-## Adds physical buttons to a connected home system - buttons hit the API endpoints covered in Pimoroni's Mote/Homekit tutorial
+## Add physical buttons to a connected home system - buttons hit the API endpoints covered in Pimoroni's Mote/Homekit tutorial
 
 from gpiozero import Button
 import time
@@ -10,7 +10,7 @@ off_button = Button(20)
 while True:
     if on_button.is_pressed == True:
 	req = urllib.urlopen('http://192.168.1.104:5000/blinkt/api/v1.0/on')
-	time.sleep(0.3)	
+	time.sleep(0.3)
     elif off_button.is_pressed == True:
 	req = urllib.urlopen('http://192.168.1.104:5000/blinkt/api/v1.0/off')
 	time.sleep(0.3)
