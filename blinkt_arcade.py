b25d6c2a8f5a884dcc1b8f4bbe2fabd4fe05ec8c
Callum Kirkwood <ckirkwood@users.noreply.github.com>
2017-04-23 10:03:41 +0100

    Update blinkt_arcade.py

--- blinkt_arcade.py
+++ blinkt_arcade.py
@@ -1,5 +1,22 @@
 ## Add physical buttons to a connected home system - buttons hit the API endpoints covered in Pimoroni's Mote/Homekit tutorial
 
+################################################################################    
+#    Copyright (C) 2017 Callum Kirkwood                                        #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+################################################################################
+
 from gpiozero import Button
 import time
 import urllib
