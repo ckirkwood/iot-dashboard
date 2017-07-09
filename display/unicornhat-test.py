1a617ed8aa6bbf2df7bbf6841925e695bb90ef83
ckirkwood <callum.kirkwood@gmail.com>
2017-07-09 18:28:24 +0000

    send to atom

--- display/unicornhat-test.py
+++ display/unicornhat-test.py
@@ -30,30 +30,30 @@ unicornhathd.brightness(0.5)
 
 width, height = unicornhathd.get_shape()
 
-try:
+tick = 1
+cross = 2
 
-    def image(img):
-        img = Image.open(img)
-        while True:
-            for o_x in range(int(img.size[0]/width)):
-                for o_y in range(int(img.size[1]/height)):
-
-                    valid = False
-                    for x in range(width):
-                        for y in range(height):
-                            pixel = img.getpixel(((o_x*width)+y,(o_y*height)+x))
-                            r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
-                            if r or g or b:
-                                valid = True
-                                unicornhathd.set_pixel(x, y, r, g, b)
-                                if valid:
-                                    unicornhathd.show()
-
+def image(png):
+    img = Image.open(png)
     while True:
-        image('tick.png')
-        time.sleep(2)
-        image('cross.png')
-        time.sleep(2)
+        for o_x in range(int(img.size[0]/width)):
+            for o_y in range(int(img.size[1]/height)):
+                valid = False
+                for x in range(width):
+    	            for y in range(height):
+                        pixel = img.getpixel(((o_x*width)+y,(o_y*height)+x))
+                        r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+                        if r or g or b:
+                            valid = True
+                            unicornhathd.set_pixel(x, y, r, g, b)
+                            if valid:
+                                unicornhathd.show()
+				time.sleep(2)
+
+try:
+    image('tick.png')
+    time.sleep(1)
+    image('cross.png')
 
 except KeyboardInterrupt:
     unicornhathd.off()
