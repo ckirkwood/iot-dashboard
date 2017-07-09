8d493da0475c32415b0fcd2ee3df94e6cf08a991
Callum Kirkwood <CallumKirkwood@192.168.1.102>
2017-07-09 19:45:59 +0100

    send to pi

--- display/unicornhat-test.py
+++ display/unicornhat-test.py
@@ -34,26 +34,28 @@ tick = 1
 cross = 2
 
 def image(png):
-    img = Image.open(png)
-    while True:
-        for o_x in range(int(img.size[0]/width)):
-            for o_y in range(int(img.size[1]/height)):
-                valid = False
-                for x in range(width):
-    	            for y in range(height):
-                        pixel = img.getpixel(((o_x*width)+y,(o_y*height)+x))
-                        r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
-                        if r or g or b:
-                            valid = True
-                            unicornhathd.set_pixel(x, y, r, g, b)
-                            if valid:
-                                unicornhathd.show()
-				time.sleep(2)
+    try:
+        while True:
+            for o_x in range(int(img.size[0]/width)):
+                for o_y in range(int(img.size[1]/height)):
+                    valid = False
+                    for x in range(width):
+    	                for y in range(height):
+                            pixel = img.getpixel(((o_x*width)+y,(o_y*height)+x))
+                            r, g, b = int(pixel[0]),int(pixel[1]),int(pixel[2])
+                            if r or g or b:
+                                valid = True
+                                unicornhathd.set_pixel(x, y, r, g, b)
+                                if valid:
+                                    unicornhathd.show()
+		    		                time.sleep(2)
+    except KeyboardInterrupt:
+        unicornhathd.off()
 
-try:
-    image('tick.png')
-    time.sleep(1)
-    image('cross.png')
-
-except KeyboardInterrupt:
-    unicornhathd.off()
+while True:
+    img = Image.open('tick.png')
+    image(img)
+    time.sleep(2)
+    img = Image.open('cross.png')
+    image(img)
+    time.sleep(2)
